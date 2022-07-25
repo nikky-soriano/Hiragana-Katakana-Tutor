3f803c784fad20b9797c5482ea0fa9ec9f0b1dee
Nikky Soriano <57335776+nikky-soriano@users.noreply.github.com>
2022-07-25 12:13:28 -0400

    Updates

---
+++ precache-manifest.c0185a255a01c2e00f0ee4b0733c09e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02057d9300830181730",
+    "url": "/Hiragana-Katakana-Tutor/static/css/main.b697e8d6.chunk.css"
+  },
+  {
+    "revision": "e02057d9300830181730",
+    "url": "/Hiragana-Katakana-Tutor/static/js/main.855c66e3.chunk.js"
+  },
+  {
+    "revision": "2ca8842ba30ae4df439e",
+    "url": "/Hiragana-Katakana-Tutor/static/js/runtime~main.8d971bcd.js"
+  },
+  {
+    "revision": "96996bd90d66b5f8dfe7",
+    "url": "/Hiragana-Katakana-Tutor/static/js/2.32e2dedf.chunk.js"
+  },
+  {
+    "revision": "753572a9cb41b8b50a1363981dc4ad7b",
+    "url": "/Hiragana-Katakana-Tutor/static/media/albanana.753572a9.png"
+  },
+  {
+    "revision": "783c43e33c05fcf81bb66eada101b19b",
+    "url": "/Hiragana-Katakana-Tutor/static/media/albanana2.783c43e3.png"
+  },
+  {
+    "revision": "30e3f8f7174a016ed95f6a063e681e49",
+    "url": "/Hiragana-Katakana-Tutor/static/media/albanana3.30e3f8f7.png"
+  },
+  {
+    "revision": "9c74fab48d720b621207cda653af9caf",
+    "url": "/Hiragana-Katakana-Tutor/static/media/sanbean.9c74fab4.png"
+  },
+  {
+    "revision": "ca4dc20366d7b71b99dab643a45217b1",
+    "url": "/Hiragana-Katakana-Tutor/index.html"
+  }
+];
